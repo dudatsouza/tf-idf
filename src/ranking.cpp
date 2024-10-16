2bf7a0d9dcfcf5a96c151a44340cb38c412e93bb
Matheus Emanuel <memanuel643@gmail.com>
2024-10-16 20:09:47 -0300

    functions' declarations and implementations

--- src/ranking.cpp
+++ src/ranking.cpp
@@ -1,12 +1,45 @@
 #include "ranking.hpp"
 
-void Ranking::readFile(const std::string &filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: could not open file " << filename << std::endl;
-        return;
+void Ranking::readFile(const std::string& filename, std::list<std::string>& words) {
+    std::ifstream arquivo(filename);
+    std::string palavra;
+    
+    if (arquivo.is_open()) {
+        while (arquivo >> palavra) {
+            words.push_back(normalize(palavra));         }
+        arquivo.close();
+    } else {
+        std::cerr << "Erro ao abrir o arquivo: " << filename << std::endl;
     }
+}
 
-    std::string line;
+std::string Ranking::normalize(const std::string &word) {
+    std::string normalized = word;
+    
+    std::transform(normalized.begin(), normalized.end(), normalized.begin(), ::tolower);
+    normalized.erase(std::remove_if(normalized.begin(), normalized.end(), [](char c) { return !std::isalnum(c) || ::ispunct(c); }), normalized.end());
 
+    return normalized;
+}
+
+int Ranking::calculateTF(const std::string &word, const std::list<std::string> &words) {
+    int count = 0;
+    
+    for (const auto &w : words) {
+        if (w == word) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+double Ranking::calculateIDF(const std::string& term) {
+    int numDocsWithTerm = 0;
+    for (const auto& doc : documents) {
+        if (std::find(doc.begin(), doc.end(), term) != doc.end()) {
+            numDocsWithTerm++;
+        }
+    }
+    return log(static_cast<double>(documents.size()) / (1 + numDocsWithTerm));
 }
--- src/ranking.hpp
+++ src/ranking.hpp
@@ -2,10 +2,29 @@
 #define RANKING_HPP
 
 #include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <unordered_map>
+#include <vector>
+#include <cmath>
+#include <cctype> 
 
 class Ranking {
+
+private:
+        std::vector<std::string> documents;
+        std::unordered_map<std::string, double> tfidf;
+
 public:
-    void readFile(const std::string &filename);
+    void readFile(const std::string &filename, std::list<std::string> &words);
+    std::string normalize(const std::string &word);
+    int calculateTF(const std::string &word, const std::list<std::string> &words);
+    double calculateIDF(const std::string &word);
+    double calculateRelevance(const std::list<std::string>& searchWords, const std::list<std::string> &document);
+    // void quickSort(std::vector<std::string> &documentos, int left, int right);
+
 
 };
 
