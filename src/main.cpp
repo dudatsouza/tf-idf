2f27e96342779187184b9dde86a6746086ca269c
dudatsouza <dudateixeirasouza@gmail.com>
2024-10-31 21:04:34 -0300

    Cálculo do Tempo de Execução e Tópicos Finais do README

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,35 @@
 
 int main() {
     std::vector<std::string> phrases = {
-        "A Quincas Borba.",
-        "A fé pode mover montanhas.",
-        "Valença.",
-        "Deus e a Terra",
-        "amor vida morte",
-        "leite café pão",
-        "grande alma pequeno corpo",
+        "A Quincas Borba",
+        "Senhor Jesus Cristo",
         "Reino de Valença",
         "hoje o dia está nublado",
         "Rubião fitava a enseada,--eram oito horas da manhã",
-        "O amor pode ser a força que une, mas também a que destrói, levando à traição e ao sofrimento.",
-        "A filosofia de Quincas Borba ensina que a vida é uma batalha constante entre o humanitismo e o egoísmo.",
-        "A fé pode mover montanhas e realizar milagres, trazendo esperança àqueles que creem.",
         "O terremoto no Reino de Valença foi uma tragédia que marcou a história, deixando um rastro de destruição e desespero.",
-        "pusillanime aborrecivel cypreste",
-        "Senhor Jesus Cristo",
         "grinaldas alcovas Nero autópsias",
-        "cadeirinha cahiu Humanitas sufficientemente",
-        "asseio compostura respeitabilidade houverdes sertão",
-        "lisboa madri espantosos Cameraria"};
+    };
 
+    // limpando terminal
+    system("clear");
+
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "RANQUEAMENTO DE DOCUMENTOS" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+    
+    if(OPTIMIZE)
+        std::cout << "Por padrão o programa está configurado para uma forma otimizada de busca, onde a frequência de palavras é armazenada em um unordered_map. Para mudar para a forma não otimizada, altere a variável OPTIMIZE para 0 no arquivo 'src/document.hpp' e recompile o programa." << std::endl << std::endl;
+    else
+        std::cout << "Por padrão o programa está configurado para uma forma não otimizada de busca, onde a frequência de palavras é armazenada em uma lista. Para mudar para a forma otimizada, altere a variável OPTIMIZE para 1 no arquivo 'src/document.hpp' e recompile o programa." << std::endl << std::endl;
+
+    std::cout << "--------------------------" << std::endl;
+
+    std::cout << "Rankeando..." << std::endl << std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    int i = 1;
     for (const auto phrase : phrases) {
-        std::cout << "Busca: " << phrase << std::endl;
+        std::cout << "Busca " << i << ": " << phrase << std::endl;
 
         Ranking ranking(phrase);
 
@@ -42,11 +48,24 @@ int main() {
         ranking.quickSort(ranking.documents, 0, ranking.documents.size() - 1);
 
         for (const auto &doc : ranking.documents) {
-            std::cout << "Documento: " << doc.filename << " - Relevância: " << doc.relevance << std::endl;
+            if (doc.relevance == 0) 
+                continue;
+
+            std::cout << "Doc: " << doc.filename << " - Relevância: " << doc.relevance << std::endl;
         }
 
+        // std::cout << "Documento: " << ranking.documents.front().filename << " - Relevância: " << ranking.documents.front().relevance << std::endl;
+
         std::cout << std::endl;
+        
+        i++;
     }
+
+    std::cout << "--------------------------" << std::endl;
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Tempo de execução: " << duration.count() / 60000 << " minutos, " << (duration.count() % 60000) / 1000 << " segundos e " << (duration.count() % 60000) % 1000 << " milissegundos" << std::endl << std::endl;
 
     return 0;
 }
